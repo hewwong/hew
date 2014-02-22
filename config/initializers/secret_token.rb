8f19e38fae952760fca3f98f8472c6240c5b1128
hew <olaio@qq.com>
2014-02-22 20:46:43 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hew::Application.config.secret_key_base = '4697e7db4b511de181e4e74be2173ee9c7b823e25d0a19331e0d2654c87aedf2be7c92c88fb5d59df88a0dbc19e9e714625d6e94b238e3977cbbfdc9524f73e9'
